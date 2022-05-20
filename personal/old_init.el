102ee825ed75306756fb8c6dad1070b904a52eb2
autumn <autumn.chiu@swagup.com>
2022-05-20 13:24:40 -0400

    comment out unnecessary package-initialize

--- personal/old_init.el
+++ personal/old_init.el
@@ -1,7 +1,7 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
-(package-initialize)
+; (package-initialize)
 (unless (package-installed-p 'use-package)
   ;; only fetch the archives if you don't have use-package installed
   (package-refresh-contents)
